941eca67cb17f80ecb1458d8eb30957150eecec8
eridhobffry <eridhobffry@github.com>
2020-01-21 14:29:03 +0100

    api auth
    constants

--- src/connectivity/api.auth.js
+++ src/connectivity/api.auth.js
@@ -1,4 +1,21 @@
+import { getBaseRequestConfig } from "./base_request_config";
+import asyncFetch from "./async_fetch";
+
 export async function login(username, password) {
   // API_BASE_URL
-  const url = process.env.REACT_APP_API_URL_LOGIN;
+  const LOGIN_ENDPOINT = process.env.REACT_APP_API_URL_LOGIN;
+
+  const baseRequestConfig = getBaseRequestConfig();
+
+  const requestConfig = Object.assign({}, baseRequestConfig, {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password
+    })
+  });
+
+  const response = await asyncFetch(LOGIN_ENDPOINT, requestConfig);
+
+  return await response.json();
 }
