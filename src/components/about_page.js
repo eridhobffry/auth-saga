1e05ab78f64e11169489841295bb7279f118eb28
eridhobffry <eridhobffry@github.com>
2020-01-21 13:34:02 +0100

    home not found about

--- src/components/about_page.js
+++ src/components/about_page.js
@@ -9,10 +9,7 @@ const AboutPage = () => {
       <h2 className="alt-header">About</h2>
       <p>
         This example app is part of the{" "}
-        <a href="https://github.com/coryhouse/react-slingshot">
-          React-Slingshot starter kit
-        </a>
-        .
+        <a href="https://github.com/eridhobffry">Eridho Buffery Rollian</a>.
       </p>
       <p>
         <Link to="/badlink">Click this bad link</Link> to see the 404 page.
---
+++ src/components/profile_area.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProfileArea = props => {
+  return (
+    <div>
+      <h1>Profile for {props.username}</h1>
+      <h3>Your pass: {props.email}</h3>
+    </div>
+  );
+};
+
+ProfileArea.PropTypes = {
+  username: PropTypes.string.isRequired
+};
+
+export default ProfileArea;
