2c0d0b03df2559e7d17c18c9853ba122f5894009
eridhobffry <eridhobffry@github.com>
2020-01-21 13:22:33 +0100

    Login Form
    About Page

---
+++ src/components/login_form.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { Button } from "reactstrap";
+import PropTypes from "prop-types";
+
+const LoginForm = props => {
+  return (
+    <form onSubmit={props.handleSubmit} className="form-signin">
+      <Field
+        component="input"
+        name="username"
+        id="username"
+        type="text"
+        placeholder="username"
+        required="required"
+        className="form-control"
+      />
+
+      <Field
+        component="input"
+        name="password"
+        id="password"
+        type="password"
+        placeholder="password"
+        required="required"
+        className="form-control"
+      />
+
+      <Button type="submit" size="lg" block color="success">
+        {props.isSubmitting ? (
+          <span>
+            <i className="fa fa-spin fa-spinner" />
+            Loging in
+          </span>
+        ) : (
+          <span>Login</span>
+        )}
+      </Button>
+    </form>
+  );
+};
+
+loginForm.PropTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  isSubmitting: PropTypes.func.isRequired
+};
+
+export default reduxForm({
+  form: "login"
+})(LoginForm);
